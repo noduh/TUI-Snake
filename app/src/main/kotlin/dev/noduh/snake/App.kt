eaae00bbf80790a497f75a4128f6222f1554bd83
Noah Jorgensen <njorgensen@byui.edu>
2026-05-29 10:18:36 -0600

    removed more autogenerated stuff and began getting needed info for the game

--- app/src/main/kotlin/dev/noduh/snake/App.kt
+++ app/src/main/kotlin/dev/noduh/snake/App.kt
@@ -3,17 +3,12 @@ package dev.noduh.snake
 import com.varabyte.kotter.foundation.session
 import com.varabyte.kotter.foundation.text.textLine
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
-
 fun main() {
     session {
         section {
-            textLine(App().greeting)
+            val terminalWidth = terminalSize.width
+            val terminalHeight = terminalSize.height
+            textLine("your terminal is ${terminalWidth}x${terminalHeight}")
         }.run()
     }
 }

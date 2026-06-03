8f188e41e6ace5a8817005e45cc4045c632b4a1e
Noah Jorgensen <njorgensen@byui.edu>
2026-06-03 14:42:13 -0600

    updated Direction and added essential functions for Snake

--- app/src/main/kotlin/dev/noduh/snake/App.kt
+++ app/src/main/kotlin/dev/noduh/snake/App.kt
@@ -14,7 +14,29 @@ fun main() {
 }
 
 enum class Direction {
-    NORTH, EAST, SOUTH, WEST
+    NORTH, EAST, SOUTH, WEST;
+
+    val opposite: Direction
+        get() = when (this) {
+            NORTH -> SOUTH
+            EAST -> WEST
+            SOUTH -> NORTH
+            WEST -> EAST
+        }
+
+    val xDirection: Int
+        get() = when (this) {
+            EAST -> 1
+            WEST -> -1
+            else -> 0
+        }
+
+    val yDirection: Int
+        get() = when (this) {
+            SOUTH -> 1
+            NORTH -> -1
+            else -> 0
+        }
 }
 
 class Snake {
@@ -24,15 +46,41 @@ class Snake {
         private set
     var headLocation: Pair<Int, Int> = Pair(0, 0)
         private set
-    private val tail: MutableList<Pair<Int, Int>> = mutableListOf<Pair<Int, Int>>()
+    private val tail: ArrayDeque<Pair<Int, Int>> = ArrayDeque<Pair<Int, Int>>()
     private var growing: Int = 0 // zero if not growing, amount left to grow otherwise
 
     fun grow(lengthToGrow: Int) {
         this.growing = lengthToGrow
     }
+
+    fun turn(tryDirection: Direction) {
+        direction = if (tryDirection == direction.opposite) direction else tryDirection
+    }
+
+    fun move() {
+        tail.addFirst(headLocation) // must happen *before* updating the headLocation
+        headLocation = Pair(
+            headLocation.first + direction.xDirection,
+            headLocation.second + direction.yDirection
+        ) // get the direction it's moving and add it to the current coordinates
+
+        if (growing != 0) {
+            growing--
+        } else {
+            tail.removeLastOrNull()
+        }
+    }
+
+    fun eat(growAmount: Int) {
+        growing += growAmount
+    }
 }
 
-class GameBoard(width: Int, height: Int) {
+class GameMap(width: Int, height: Int) {
     private val board: Array<Array<Int>> = Array(width) { Array(height) { 0 } }
 
+}
+
+class Game() {
+    //
 }

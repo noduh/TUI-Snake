79342a5cd509565ece7892dec425d2f5cdfcd532
Noah Jorgensen <njorgensen@byui.edu>
2026-06-05 13:53:43 -0600

    Implemented some GameMap functionality and added BoardPiece to improve logic

--- app/src/main/kotlin/dev/noduh/snake/App.kt
+++ app/src/main/kotlin/dev/noduh/snake/App.kt
@@ -8,8 +8,11 @@ fun main() {
         section {
             val terminalWidth = terminalSize.width
             val terminalHeight = terminalSize.height
+
             textLine("your terminal is ${terminalWidth}x${terminalHeight}")
-        }.run()
+        }.run {
+
+        }
     }
 }
 
@@ -39,14 +42,41 @@ enum class Direction {
         }
 }
 
+enum class BoardPiece { // defines what needs to be drawn on the board
+    EMPTY, APPLE, HEAD, TAIL, END;
+
+    val isSnake: Boolean
+        get() = when (this) {
+            HEAD -> true
+            TAIL -> true
+            END -> true
+            else -> false
+        }
+
+    val isHead: Boolean
+        get() = this == HEAD
+
+    val isTail: Boolean
+        get() = this == TAIL
+
+    val isEnd: Boolean
+        get() = this == END
+
+    val isApple: Boolean
+        get() = this == APPLE
+
+    val isEmpty: Boolean
+        get() = this == EMPTY
+}
+
 class Snake {
-    var length: Int = 0
+    var length: Int = 2 // this includes head and end
         private set
     var direction: Direction = Direction.EAST
         private set
     var headLocation: Pair<Int, Int> = Pair(0, 0)
         private set
-    private val tail: ArrayDeque<Pair<Int, Int>> = ArrayDeque<Pair<Int, Int>>()
+    private val tail: ArrayDeque<Triple<Int, Int, Direction>> = ArrayDeque<Triple<Int, Int, Direction>>()
     private var growing: Int = 0 // zero if not growing, amount left to grow otherwise
 
     fun grow(lengthToGrow: Int) {
@@ -58,7 +88,13 @@ class Snake {
     }
 
     fun move() {
-        tail.addFirst(headLocation) // must happen *before* updating the headLocation
+        tail.addFirst(
+            Triple(
+                headLocation.first,
+                headLocation.second,
+                direction
+            )
+        ) // must happen *before* updating the headLocation
         headLocation = Pair(
             headLocation.first + direction.xDirection,
             headLocation.second + direction.yDirection
@@ -74,11 +110,27 @@ class Snake {
     fun eat(growAmount: Int) {
         growing += growAmount
     }
+
+    fun getTail(): Array<Triple<Int, Int, Direction>> = tail.toTypedArray() // don't want to pass the mutable data
 }
 
 class GameMap(width: Int, height: Int) {
-    private val board: Array<Array<Int>> = Array(width) { Array(height) { 0 } }
+    private val board: Array<Array<BoardPiece>> = Array(width) { Array(height) { BoardPiece.EMPTY } }
+    val dimensions: Pair<Int, Int> = Pair(width, height)
+    private val snake: Snake = Snake()
 
+    fun spawnApple() {
+        val maxX: Int = dimensions.first - 1
+        val maxY: Int = dimensions.second - 1
+        var tryLocation: Pair<Int, Int> = Pair(-1, -1)
+        var pieceAtLocation: BoardPiece = BoardPiece.EMPTY
+        do {
+            tryLocation = Pair(((0..maxX).random()), ((0..maxY).random()))
+            pieceAtLocation = board[tryLocation.first][tryLocation.second]
+        } while (pieceAtLocation.isSnake)
+
+        board[tryLocation.first][tryLocation.second] = BoardPiece.APPLE
+    }
 }
 
 class Game() {
